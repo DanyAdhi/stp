d8a985989cc80a022e8dcdb08b1f4a9e0d0661ad
danyadhi <danyadhi4149@gmail.com>
2023-07-04 08:17:38 +0700

    update feature dashboard member

--- application/controllers/peserta/Dashboard.php
+++ application/controllers/peserta/Dashboard.php
@@ -6,10 +6,16 @@ class Dashboard extends CI_Controller {
   public function __construct() {
     parent::__construct();
     if ($this->session->userdata('role_id') !== 2) { redirect('auth'); }
+
+    $this->load->model(['Peserta/M_dashboard']);
   }
 
   public function index() {
-    $data = [ 'content' => 'peserta/dashboard/index', ];
+
+    $data = [ 
+      'content'   => 'peserta/dashboard/index',
+      'progress'  => $this->M_dashboard->get_progress_member($this->session->userdata('member_id')),
+    ];
 		$this->load->view('peserta/template/Template', $data);
   }
 
